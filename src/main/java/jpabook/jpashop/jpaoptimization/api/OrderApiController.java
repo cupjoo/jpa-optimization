82fa2d0c5e062c3574b92110dcd753645d0ef58b
Unknown <cupjoo@naver.com>
2020-04-11 19:40:27 +0900

    add order api controller

---
+++ src/main/java/jpabook/jpashop/jpaoptimization/api/OrderApiController.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.jpaoptimization.api;
+
+import jpabook.jpashop.jpaoptimization.domain.Address;
+import jpabook.jpashop.jpaoptimization.domain.Order;
+import jpabook.jpashop.jpaoptimization.domain.OrderSearch;
+import jpabook.jpashop.jpaoptimization.domain.OrderStatus;
+import jpabook.jpashop.jpaoptimization.repository.OrderRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        return orderRepository.findAllByString(new OrderSearch()).stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+}
